54212d07e883ee314f4d9f18fb0b4c01ccc3fdf5
restimirandaNU <43328718+restimirandaNU@users.noreply.github.com>
2018-12-22 20:51:42 -0600

    Manually Uploaded Files
    Manually Uploaded Files

---
+++ index.js
@@ -0,0 +1,106 @@
+var Word = require("./word.js");
+var inquirer = require("inquirer")
+
+var wordArray = ["variable", "integer", "html", "css", "jquery", "javascript", "object", "bootstrap", "api", "firebase", "inheritance", "react", "node", "constructor", ];
+var randomWord = "";
+var displayWerd = "";
+var finalWerd;
+var leftToGuess;
+var lives = 6
+
+function newGame() {
+    randomWord = "";
+    var r = parseInt(Math.floor(Math.random() * (wordArray.length)))
+    randomWord = wordArray[r]
+    finalWerd = new Word(randomWord)
+    leftToGuess = finalWerd.letterArr.length
+}
+
+function gameOver() {
+    {
+        console.log("Game over.")
+        inquirer.prompt([{
+            type: "confirm",
+            name: "playAgain",
+            message: "Would you like to play again?"
+        }]).then(function (response) {
+            if (response.playAgain) {
+                newGame()
+                print()
+                askToGuess();
+            } else {
+                console.log("Great! Thanks for player")
+            }
+        })
+    }
+}
+
+function displayWord() {
+    displayWerd = finalWerd.createWerdString()
+    console.log(displayWerd);
+    finalWerd.compare = displayWerd
+}
+
+
+function askToGuess() {
+    inquirer.prompt([{
+        name: "ask",
+        message: "Guess a Coding term! Pick a letter"
+    }]).then(function (response) {
+        var input = response.ask
+        if (lives > 0) {
+            if (input.length === 1) {
+                finalWerd.guessCheck(input)
+                displayWerd = finalWerd.createWerdString()
+
+                if (finalWerd.compare === displayWerd) {
+                    console.log("Nope, there is no", input, "in the word")
+                    lives--
+                    console.log("You have", lives, "guesse(s) remaining.")
+                    if (lives === 0) {
+                        gameOver()
+                    } else {
+                        print()
+                        askToGuess()
+                    }
+                   
+                } else {
+                    console.log("Great pick!")
+                    leftToGuess--
+                    print();
+                    if (leftToGuess === 0) {
+
+                        console.log("Great Job! Here's the next word:");
+                        newGame()
+                        print();
+                        askToGuess();
+                    } else {
+                        askToGuess()
+                    }
+                }
+
+            } else if (input.length === 0) {
+                consoel.log("Please choose a letter.");
+                askToGuess()
+            } else {
+                console.log("Pick one letter a a time please.")
+                askToGuess()
+            }
+
+
+        } else {
+            gameOver()
+        }
+    })
+}
+
+function print() {
+    console.log("\n")
+    console.log("******************************************")
+    displayWord()
+    console.log("\n*****************************************")
+    console.log("\n")
+}
+newGame()
+print()
+askToGuess();
